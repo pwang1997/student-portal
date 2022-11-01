e4745fe2e534e782927929f9651015e991c7ba55
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-10-31 22:38:31 -0400

    add home.html

--- student/views.py
+++ student/views.py
@@ -22,3 +22,7 @@ def add(request):
 def search(request):
     template = loader.get_template('search.html')
     return HttpResponse(template.render())
+
+def home(request):
+    template = loader.get_template('home.html')
+    return HttpResponse(template.render())
