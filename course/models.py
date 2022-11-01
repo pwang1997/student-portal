c62bfeffdc0eda9a0c1ae2923602c829e1e018a6
hkara016 <113481802+hkara016@users.noreply.github.com>
2022-10-31 21:44:50 -0400

    Update models.py

--- course/models.py
+++ course/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 
-# Create your models here.
-
-#todo: create course model
+class Course (models.Model):
+    id = models.IntegerField()
+    description = models.CharField(max_length=100)
+    prerequisite = models.CharField(max_length=100)
+    department = models.CharField(max_length=50)
+    code = models.IntegerField()
+    section = models.IntegerField()
