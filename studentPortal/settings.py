0d7c5ef7fe49e99d3838a6a044353e156c226cd7
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-11-03 23:39:34 -0400

    update home page header section

--- studentPortal/settings.py
+++ studentPortal/settings.py
@@ -10,11 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'student/static'),]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -59,7 +61,7 @@ ROOT_URLCONF = 'studentPortal.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'student', 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
