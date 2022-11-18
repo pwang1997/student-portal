07a60e224e9fd1a88d5ac8fe12e63648abb05f01
hkara016 <113481802+hkara016@users.noreply.github.com>
2022-11-18 17:39:50 -0500

    Update views.py

--- schoolAdmin/views.py
+++ schoolAdmin/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from student.models import Student
+from course.models import Course
+from professor.models import Professor
 
 # Create your views here.
 def index(request):
@@ -24,7 +27,7 @@ def add_course(request):
         code = request.POST["code"]
         section = request.POST["section"]
         
-        course_instance = course(description=description, prerequisite=prerequisite, department=department, code=code, section=section)
+        course_instance = Course(description=description, prerequisite=prerequisite, department=department, code=code, section=section)
         course_instance.save()
         
         return HttpResponse (200)
@@ -35,7 +38,7 @@ def add_course(request):
 
 
 def update_course(request, course_id):
-  Update_course_instance = course.objects.get(id=course_id)
+  Update_course_instance = Course.objects.get(id=course_id)
   template = loader.get_template('update.html')
   context = {
     'Update_course_instance': Update_course_instance,
@@ -45,7 +48,7 @@ def update_course(request, course_id):
 
 
 def delete_course(request, course_id):
-    course_delete_instance = course.objects.get(id=course_id)
+    course_delete_instance = Course.objects.get(id=course_id)
     try:
         course_delete_instance.delete()
         return HttpResponse (200)
@@ -59,7 +62,7 @@ def add_student(request):
     if request.method == 'POST':
         major= request.POST["major"]
         
-        student_instance = student(major=major)
+        student_instance = Student(major=major)
         student_instance.save()
         
         return HttpResponse (200)
@@ -69,7 +72,7 @@ def add_student(request):
         return HttpResponse (400)
 
 def update_student(request, student_id):
-  Update_student_instance = student.objects.get(id=student_id)
+  Update_student_instance = Student.objects.get(id=student_id)
   template = loader.get_template('update.html')
   context = {
     'Update_student_instance': Update_student_instance,
@@ -78,7 +81,7 @@ def update_student(request, student_id):
 
 
 def delete_student(request, student_id):
-    student_delete_instance = student.objects.get(id=student_id)
+    student_delete_instance = Student.objects.get(id=student_id)
     try:
         student_delete_instance.delete()
         return HttpResponse (200)
@@ -93,7 +96,7 @@ def add_professor(request):
         department= request.POST["department"]
         salary= request.POST["salary"]
         
-        professor_instance = professor(department=department, salary=salary)
+        professor_instance = Professor(department=department, salary=salary)
         professor_instance.save()
         
         return HttpResponse (200)
@@ -103,7 +106,7 @@ def add_professor(request):
         return HttpResponse (400)
 
 def update_professor(request, professor_id):
-  Update_professor_instance = professor.objects.get(id=professor_id)
+  Update_professor_instance = Professor.objects.get(id=professor_id)
   template = loader.get_template('update.html')
   context = {
     'Update_professor_instance': Update_professor_instance,
@@ -112,7 +115,7 @@ def update_professor(request, professor_id):
   
 
 def delete_professor(request, professor_id):
-    professor_delete_instance = professor.objects.get(id=professor_id)
+    professor_delete_instance = Professor.objects.get(id=professor_id)
     try:
         professor_delete_instance.delete()
         return HttpResponse (200)
