536502d2a94358f7e62ba4712e8a08e40af767c9
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-11-18 13:57:17 -0500

    renamed the index.html to student_home.html

--- student/urls.py
+++ student/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('drop/', views.drop, name='course'), # entrolled/ wait-list course list with a button for withdrawal
-    path('class_schdule/', views.class_schdule, name='class_schdule'), 
+    path('', views.student_home, name='student_home'),
+    #path('drop/', views.drop, name='course'), # entrolled/ wait-list course list with a button for withdrawal
+    #path('class_schdule/', views.class_schdule, name='class_schdule'), 
     path('add/', views.add, name='add'),
-    path('search/', views.search, name='search'),
+    #path('search/', views.search, name='search'),
 ]
