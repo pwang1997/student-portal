968276dc2176fe02888a3210b8403e713612a2bb
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-11-18 16:56:42 -0500

    professor_home.html created

--- professor/urls.py
+++ professor/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'), # professor dashboard
-    path('teaching/', views.index, name='index'), # professor dashboard
-    path('teaching/course', views.teaching_course, name='teaching_course'), # teaching/ taught course list
+    path('', views.professor_home, name='professor_home'), # professor dashboard
+    #path('teaching/', views.index, name='index'), # professor dashboard
+    #path('teaching/course', views.teaching_course, name='teaching_course'), # teaching/ taught course list
 ]
--- professor/views.py
+++ professor/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def index(request):
-    template = loader.get_template('index.html')
+def professor_home(request):
+    template = loader.get_template('professor_home.html')
     return HttpResponse(template.render())
 
 def teaching_course(request):
