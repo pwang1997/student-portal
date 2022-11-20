075b5d5a1d22d4b2a8cc1ca812d383b21511819e
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-11-19 21:31:18 -0500

    student_list.html added

--- professor/urls.py
+++ professor/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.professor_home, name='professor_home'), # professor dashboard
+    path('<str:course_code>/student_list', views.student_list, name='student_list')
     #path('teaching/', views.index, name='index'), # professor dashboard
     #path('teaching/course', views.teaching_course, name='teaching_course'), # teaching/ taught course list
 ]
--- professor/views.py
+++ professor/views.py
@@ -7,5 +7,6 @@ def professor_home(request):
     template = loader.get_template('professor_home.html')
     return HttpResponse(template.render())
 
-def teaching_course(request):
-    return HttpResponse("Professor: Page Teaching Course List")
+def student_list(request,course_code):
+    template = loader.get_template('student_list.html')
+    return HttpResponse(template.render())
