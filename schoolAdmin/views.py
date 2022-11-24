fcc44b493896de32407faa8609c4800606844d99
thomascourtney <64978864+thomascourtney@users.noreply.github.com>
2022-11-24 15:12:16 -0500

    front end finished

--- schoolAdmin/views.py
+++ schoolAdmin/views.py
@@ -19,5 +19,24 @@ def modify_course(request):
 def modify_professor(request):
     return render(request, 'modify-professor.html')
 
+def modify_course_nlp(request):
+    return render(request, 'modify-course-nlp.html')
+
+def modify_course_cs(request):
+    return render(request, 'modify-course-cs.html')
+
+def modify_course_fscc(request):
+    return render(request, 'modify-course-fscc.html')
+
+def modify_student_fscc(request):
+    return render(request, 'modify-student-fscc.html')
+
+def modify_student_nlp(request):
+    return render(request, 'modify-student-nlp.html')
 
 
+def modify_professor_fscc(request):
+    return render(request, 'modify-professor-fscc.html')
+
+def modify_professor_nlp(request):
+    return render(request, 'modify-professor-nlp.html')
