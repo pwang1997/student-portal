d25a5e0722a696214e2da1970a39d633884c2cda
hkara016 <113481802+hkara016@users.noreply.github.com>
2022-11-30 16:42:42 -0500

    Professor Updated
    Code includes:
    professor can view the courses he is teaching, professor can view students in a courses, professor can update student grades in a course.

--- professor/models.py
+++ professor/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class registered_courses (models.Model):
+    course_id = models.IntegerField()
+    professor_id = models.IntegerField()
+
+class registered_students_in_a_course (models.Model):
+    course_id = models.IntegerField()
+    professor_id = models.IntegerField()
+    student_id = models.IntegerField()
+    grade = models.CharField(max_length=50)
+
+    
--- professor/views.py
+++ professor/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from professor.models import registered_courses
+from professor.models import registered_students_in_a_course
+from student.models import registered_students_in_a_course
 
 # Create your views here.
 def professor_home(request):
@@ -10,3 +13,34 @@ def professor_home(request):
 def student_list(request,course_code):
     template = loader.get_template('student_list.html')
     return HttpResponse(template.render())
+
+
+
+#professor can view the courses he is teaching
+def display_course(request):
+	co = registered_courses.objects.filter(professor_id = "professor_id")
+
+	return render(request,'display.html',{'co':co})
+
+#professor can view students in a courses
+def display_students(request):
+	cs = registered_students_in_a_course.objects.filter(professor_id = "professor_id")
+
+	return render(request,'display_students.html',{'cs':cs})
+
+#professor can update student grades in a course
+def update_student_grade (request, student_id):
+  update_student_grade_instance = registered_students_in_a_course.objects.get(id=student_id)
+  template = loader.get_template('update_student_grade.html')
+  context = {
+    'update_student_grade_instance': update_student_grade_instance,
+  }
+  return HttpResponse(template.render(context, request))
+
+
+
+
+
+
+
+
