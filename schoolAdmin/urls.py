94c7b732208d4e37845f193db2d7da2ac0c5f920
thomascourtney <64978864+thomascourtney@users.noreply.github.com>
2022-11-29 19:03:42 -0500

    changes

--- schoolAdmin/urls.py
+++ schoolAdmin/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('modify-student/', views.modify_student, name='modify-student'), 
     path('modify-professor/', views.modify_professor, name='modify-professor'),
     path('modify-course/', views.modify_course, name='modify-course'),
-    path('modify-course/add-course/', views.modify_course_add, name='modify-course'),
     path('modify-course/nlp/', views.modify_course_nlp, name='modify-course-nlp'),
     path('modify-student/nlp/', views.modify_student_nlp, name="modify_student_nlp"),
     path('modify-professor/nlp/', views.modify_professor_nlp, name='modify-professor_nlp'),
