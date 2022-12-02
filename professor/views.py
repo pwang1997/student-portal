c06e66172a67f4b24ca4ac5bbe7415fb5fee0a27
hkara016 <113481802+hkara016@users.noreply.github.com>
2022-12-02 15:57:07 -0500

    Professor Branch Recent updated
    html created, urls added

--- professor/views.py
+++ professor/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from professor.models import registered_courses
-from professor.models import registered_students_in_a_course
 from student.models import registered_students_in_a_course
 
 # Create your views here.
@@ -17,20 +16,20 @@ def student_list(request,course_code):
 
 
 #professor can view the courses he is teaching
-def display_course(request):
-	co = registered_courses.objects.filter(professor_id = "professor_id")
+def display_course(request, professor_id):
+	co = registered_courses.objects.filter(professor_id=professor_id)
 
-	return render(request,'display.html',{'co':co})
+	return render(request,'display_course.html',{'co':co})
 
 #professor can view students in a courses
-def display_students(request):
-	cs = registered_students_in_a_course.objects.filter(professor_id = "professor_id")
+def display_students(request, professor_id):
+	cs = registered_students_in_a_course.objects.filter(professor_id=professor_id)
 
 	return render(request,'display_students.html',{'cs':cs})
 
 #professor can update student grades in a course
-def update_student_grade (request, student_id):
-  update_student_grade_instance = registered_students_in_a_course.objects.get(id=student_id)
+def update_student_grade (request, student_id, course_id, professor_id):
+  update_student_grade_instance = registered_students_in_a_course.objects.get(student_id=student_id, course_id=course_id, professor_id=professor_id)
   template = loader.get_template('update_student_grade.html')
   context = {
     'update_student_grade_instance': update_student_grade_instance,
