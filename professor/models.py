a7313900475a4c2520426211fb02b3b25ba14a2c
zwang1997 <zwang684@uottawa.ca>
2022-12-02 20:16:51 -0500

    move registered_students to student model

--- professor/models.py
+++ professor/models.py
@@ -6,13 +6,6 @@ from user.models import Users
 class registered_courses (models.Model):
     course_id = models.IntegerField()
     professor_id = models.IntegerField()
-
-class registered_students_in_a_course (models.Model):
-    course_id = models.IntegerField()
-    professor_id = models.IntegerField()
-    student_id = models.IntegerField()
-    grade = models.CharField(max_length=50)
-
    
 class Professor(Users):
       department = models.CharField(max_length = 255)
--- student/models.py
+++ student/models.py
@@ -4,7 +4,11 @@ from django.db import models
 from user.models import Users
 
 # Create your models here.
-
-
 class Student(Users):
     major = models.CharField(max_length = 255)
+
+class registered_students_in_a_course (models.Model):
+    course_id = models.IntegerField()
+    professor_id = models.IntegerField()
+    student_id = models.IntegerField()
+    grade = models.CharField(max_length=50)
