49cc46a78eaedcf05e3e6e0ba4b3c7e357465698
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-12-02 19:35:57 -0500

    deleted some templates which were integrated to home page

--- student/urls.py
+++ student/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('drop/', views.drop, name='course'), # entrolled/ wait-list course list with a button for withdrawal
-    path('class_schdule/', views.class_schdule, name='class_schdule'), 
+    #path('drop/', views.drop, name='course'), # entrolled/ wait-list course list with a button for withdrawal
+    #path('class_schdule/', views.class_schdule, name='class_schdule'), 
     path('add/', views.add, name='add'),
-    path('search/', views.search, name='search'),
+    #path('search/', views.search, name='search'),
 ]
