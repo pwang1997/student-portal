c826bba97c2924d76c7d24f0c941c5b3e9253e91
zwang684 <puck19970418@gmail.com>
2022-12-02 23:08:14 -0500

    fix issue: admin-login not found

--- schoolAdmin/views.py
+++ schoolAdmin/views.py
@@ -15,7 +15,7 @@ def index(request):
     return HttpResponse(template.render())
 
 def admin_login(request):
-    return render(request, 'admin-login.html')
+    return render(request, 'login.html')
 
 def modify_student(request):
     return render(request, 'modify-student.html')
