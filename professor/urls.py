5374180eaf1c29b105ef0182a2cfe2bd9bfb52a1
Xiong Gu <xionggu@CindydeMacBook-Pro.local>
2022-12-05 00:44:22 -0500

    professor view student list function finished

--- professor/urls.py
+++ professor/urls.py
@@ -5,7 +5,5 @@ from student import handler
 urlpatterns = [
     path('', views.professor_home, name='professor_home'), # professor dashboard
     path('logout', handler.logout, name='logout'),
-    path('<str:course_code>/student_list', views.student_list, name='student_list')
-    #path('teaching/', views.index, name='index'), # professor dashboard
-    #path('teaching/course', views.teaching_course, name='teaching_course'), # teaching/ taught course list
+    path('student_list/<int:id>', views.student_list, name='student_list')
 ]
--- professor/views.py
+++ professor/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest
 from django.template import loader
 from professor.models import registered_courses,Professor
-from student.models import Enrolled_courses_of_a_student
+from student.models import Enrolled_courses_of_a_student, Student
 from course.models import Course
 
 # Create your views here.
@@ -15,27 +15,27 @@ def professor_home(request):
         return HttpResponseBadRequest("Invalid User Role", status=400)
     professor = Professor.objects.get(id=request.session['user']['id'])
     professor_email = professor.email
+    #professor can view the courses he is teaching
     teaching_courses = Course.objects.filter(instructor=professor_email)
     context["teaching_courses"] = teaching_courses
 
     # return HttpResponse(template.render())
     return render(request, 'professor_home.html', context)
 
-def student_list(request,course_code):
-    template = loader.get_template('student_list.html')
-    return HttpResponse(template.render())
+def student_list(request,id):
+    context ={}
+    professor = Professor.objects.get(id=request.session['user']['id'])
+    course = Course.objects.get(id=id)
+    enrolled_students = Enrolled_courses_of_a_student.objects.filter(course_id=course.id,professor_id=professor.id)
+    student_list = []
+    for item in enrolled_students:
+        student = Student.objects.get(id=item.student_id)
+        student_list.append((student,item.grade))
+    context["student_list"] = student_list
+    context["course"] = course
 
-#professor can view the courses he is teaching
-def display_course(request, professor_id):
-	co = registered_courses.objects.filter(professor_id=professor_id)
+    return render(request, 'student_list.html', context)
 
-	return render(request,'display_course.html',{'co':co})
-
-#professor can view students in a courses
-def display_students(request, professor_id):
-	cs = Enrolled_courses_of_a_student.objects.filter(professor_id=professor_id)
-
-	return render(request,'display_students.html',{'cs':cs})
 
 #professor can update student grades in a course
 def update_student_grade (request, student_id, course_id, professor_id):
